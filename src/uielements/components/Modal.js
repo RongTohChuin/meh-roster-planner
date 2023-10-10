fd266198f5794b6f82b7dfece2718419603cdfa8
RongTohChuin <rongchuin@gmail.com>
2023-10-11 00:52:36 +0800

    adding login page

--- src/uielements/components/Modal.js
+++ src/uielements/components/Modal.js
@@ -5,8 +5,22 @@ import "./Modal.css";
 const Login = () => {
   return (
     <div className="login-fields">
-      <input type="text" className="login-inputs__username"></input>
-      <input type="text" className="login-inputs__password"></input>
+      <div className="login-inputs__username">
+        <h3 className="login-inputs__username-label">Username:</h3>
+        <input
+          type="text"
+          className="login-inputs__username"
+          id="login-username"
+        ></input>
+      </div>
+      <div className="login-inputs__password">
+        <h3 className="login-inputs__password-label">Password:</h3>
+        <input
+          type="text"
+          className="login-inputs__password"
+          id="login-password"
+        ></input>
+      </div>
       <button className="login-button">LOGIN</button>
     </div>
   );
