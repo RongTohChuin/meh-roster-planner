0d8f6fb16722feebafd6e5764fbc129d9a5cbcd7
rongtohchuin <rongchuin@gmail.com>
2023-10-10 11:29:26 +0800

    First commit!

---
+++ src/uielements/components/Modal.js
@@ -0,0 +1,8 @@
+import React,{ReactPortal} from "react";
+import "./Modal.css";
+
+const ModalBackground = (props) => {
+  return <div className="modal-background"></div>;
+};
+
+export default ModalBackground;
